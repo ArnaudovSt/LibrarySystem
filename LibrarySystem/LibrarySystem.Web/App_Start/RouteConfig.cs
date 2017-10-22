92c832b45a114d5c440f2d3e219f8486945d5dff
ArnaudovSt <telerikacademy2016@gmail.com>
2017-10-22 04:46:50 +0300

    added book details

--- LibrarySystem/LibrarySystem.Web/App_Start/RouteConfig.cs
+++ LibrarySystem/LibrarySystem.Web/App_Start/RouteConfig.cs
@@ -16,6 +16,11 @@ namespace LibrarySystem.Web
             routes.LowercaseUrls = true;
 
             routes.MapRoute(
+                name: "Book",
+                url: "book/{id}",
+                defaults: new { controller = "Book", action = "BookDetails" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- LibrarySystem/LibrarySystem.Web/ViewModels/Book/BookDetailsViewModel.cs
+++ LibrarySystem/LibrarySystem.Web/ViewModels/Book/BookDetailsViewModel.cs
@@ -9,6 +9,22 @@ namespace LibrarySystem.Web.ViewModels.Book
 {
     public class BookDetailsViewModel : IMapFrom<Data.Models.Book>
     {
-        public ICollection<Genre> Genres { get; set; }
+        public ICollection<Author> Authors;
+
+        public ICollection<Genre> Genres;
+
+        public ICollection<Comment> Comments;
+
+        public ICollection<Rating> Ratings;
+
+        public string Title { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Description { get; set; }
+
+        public int PageCount { get; set; }
+
+        public int YearOfPublishing { get; set; }
     }
 }
